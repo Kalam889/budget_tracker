4e406c5f6780755735450448a439a9da7251c763
Kalam <mdkalam1905@gmail.com>
2026-04-01 00:21:02 +0530

    updated function

--- frontend/src/pages/Add.jsx
+++ frontend/src/pages/Add.jsx
@@ -8,6 +8,18 @@ function Add() {
     const [date, setDate] = useState("");
     const [note, setNote] = useState("");
 
+    const handleAdd = () => {
+        const newTransaction = {
+            amount: Number(amount),
+            category,
+            type,
+            date,
+            note
+        };
+        const oldData = JSON.parse(localStorage.getItem("transactions")) || [];
+        const updatedData = [...oldData, newTransaction];
+        localStorage.setItem("transactions", JSON.stringify(updatedData));
+}  
     return (
         <div>
             <h1>Add Transaction</h1>
@@ -15,32 +27,31 @@ function Add() {
             <button onClick={() => setType("expense")}>Expense</button>
             {type === "income" ? (
                 <>
-                <input type="number" value={amount} placeholder="Enter amount" />
+                <input type="number" value={amount} placeholder="Enter amount" onChange={(e) => setAmount(e.target.value)}/>
                 <h2>Category</h2>
                 <select value={category}>
                     <option value="salary" >Salary</option>
                     <option value="freelance">Freelance</option>
                     <option value="gift">Gift</option>
                 </select>
-                <input type="date" value={date} placeholder="Enter date" />
-                <input type="text" value={note} placeholder="Enter Notes" />
+                <input type="date" value={date} placeholder="Enter date" onChange={(e) => setDate(e.target.value)}/>
+                <input type="text" value={note} placeholder="Enter Notes" onChange={(e) => setNote(e.target.value)}/>
    
                 </>
             ) : (
                 <>
-                <input type="number" value={amount} placeholder="Enter amount" />
+                <input type="number" value={amount} placeholder="Enter amount" onChange={(e) => setAmount(e.target.value)}/>
                 <h2>Category</h2>
                 <select value={category}>
                     <option value="food">Food</option>
                     <option value="travel">Travel</option>
                     <option value="bills">Bills</option>
                 </select>
-                <input type="date" value={date} placeholder="Enter date" />
-                <input type="text" value={note} placeholder="Enter Notes" />
-
+                <input type="date" value={date} placeholder="Enter date" onChange={(e) => setDate(e.target.value)}/>
+                <input type="text" value={note} placeholder="Enter Notes" onChange={(e) => setNote(e.target.value)}/>
                 </>
             )}
-            <button>Add Transaction</button>
+            <button onClick={handleAdd}>Add Transaction</button>
              <BottomNav/>
         </div>
     )
--- frontend/src/pages/History.jsx
+++ frontend/src/pages/History.jsx
@@ -1,29 +1,28 @@
 import BottomNav from "../components/BottomNav";
-import { useState } from "react";
-const transactions = [
-        { amount: 200, category: "food", type: "expense", date: "2026-03-20", note: "Lunch" },
-        { amount: 500, category: "travel", type: "expense", date: "2026-03-21", note: "Auto" },
-        { amount: 1000, category: "salary", type: "income", date: "2026-03-22", note: "Monthly salary" },
-        { amount: 2000, category: "freelance", type: "income", date: "2026-03-23", note: "Project" },
-        { amount: 500, category: "food", type: "expense", date: "2026-03-24", note: "Dinner" },
-    ];
+// const transactions = [
+//         { amount: 200, category: "food", type: "expense", date: "2026-03-20", note: "Lunch" },
+//         { amount: 500, category: "travel", type: "expense", date: "2026-03-21", note: "Auto" },
+//         { amount: 1000, category: "salary", type: "income", date: "2026-03-22", note: "Monthly salary" },
+//         { amount: 2000, category: "freelance", type: "income", date: "2026-03-23", note: "Project" },
+//         { amount: 500, category: "food", type: "expense", date: "2026-03-24", note: "Dinner" },
+//     ];
 
 function History() {
+    const transactions = JSON.parse(localStorage.getItem("transactions")) || [];
     return (
         <>
-        <h2>Transaction History</h2>
-         {transactions.map((item, index) => (
+            <h2>Transaction History</h2>
+            {transactions.map((item, index) => (
                 <div key={index} >
-                    
+
                     <h3>{item.category.toUpperCase()}</h3>
-                    
                     <p>Amount: ₹{item.amount}</p>
                     <p>Type: {item.type}</p>
                     <p>Date: {item.date}</p>
                     <p>Note: {item.note}</p>
-                      </div>
+                </div>
             ))}
-                <BottomNav />
+            <BottomNav />
 
         </>
     )
